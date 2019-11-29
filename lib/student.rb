b540d46939c961dfcaa2f482c03fc75c66d472e9
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 04:46:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
-
+class Student < User 
+      attr_accessor:first_name,:last_name
+      def initialize
+         @knowledge = []
+      end
+      
+      def learn(take_arg)
+          @knowledge << take_arg
+         
+      end
+      
+      def knowledge
+         @knowledge
+      end
 end
